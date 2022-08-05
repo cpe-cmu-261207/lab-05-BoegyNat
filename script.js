e909b85dcaa2c7c8830403bcf7a640036848deef
BT_kung <u.nantanat@gmail.com>
2022-08-06 01:24:33 +0700

    finish

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const todoCtn = document.getElementById("todo-container");
 
 inputAdd.onkeyup = (event) => {
   if (event.key !== "Enter") return;
+  if (inputAdd.value == "") {
+    alert("todo can not be empty");
+  } else {
+    let check = false;
+    addTodo(inputAdd.value, check);
+    inputAdd.value = "";
+    saveTodo();
+  }
 
   //your code here
 };
@@ -28,6 +36,36 @@ function addTodo(title, completed) {
   deleteBtn.innerText = "Delete";
   deleteBtn.className = "btn btn-danger";
 
+  doneBtn.style.display = "none";
+  deleteBtn.style.display = "none";
+  div.appendChild(span);
+  div.appendChild(doneBtn);
+  div.appendChild(deleteBtn);
+  todoCtn.prepend(div);
+
+  div.onmouseover = () => {
+    doneBtn.style.display = "";
+    deleteBtn.style.display = "";
+  };
+  div.onmouseout = () => {
+    doneBtn.style.display = "none";
+    deleteBtn.style.display = "none";
+  };
+
+  doneBtn.onclick = () => {
+    if (span.style.textDecoration === "line-through") {
+      completed = false;
+      span.style.textDecoration = "";
+    } else {
+      completed = true;
+      span.style.textDecoration = "line-through";
+    }
+    saveTodo();
+  };
+  deleteBtn.onclick = () => {
+    todoCtn.removeChild(div);
+    saveTodo();
+  };
   //your code here
   //append todo to HTML...
   //define buttons event...
@@ -36,12 +74,27 @@ function addTodo(title, completed) {
 function saveTodo() {
   const data = [];
   for (const todoDiv of todoCtn.children) {
+    const todoObj = {};
+    console.log(todoDiv);
+    todoObj.title = todoDiv.children[0].innerText;
+
+    todoObj.completed =
+      todoDiv.children[0].style.textDecoration === "line-through";
+    data.push(todoObj);
     //your code here
   }
+  console.log(data);
+  const dataStr = JSON.stringify(data);
+  localStorage.setItem("data", dataStr);
   //your code here
 }
 
 function loadTodo() {
+  const dataStr = localStorage.getItem("Data");
+  const data = JSON.parse(dataStr);
+  for (const todoOdj in data) {
+    addTodo(todoOdj.title, todoOdj.completed);
+  }
   //your code here
 }
 
